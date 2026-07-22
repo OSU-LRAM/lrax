245644a68c7fd4cad8d7f104cb1e44fdc363208a
Evan Palmer <evanp922@gmail.com>
2026-07-22 11:16:01 -0700

    fixes

--- lrax/__init__.py
+++ lrax/__init__.py
@@ -18,6 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from .ppo import PPO
+from ._epsilon import EPSILON
 
-__all__ = ["PPO"]
+__all__ = ["EPSILON"]
