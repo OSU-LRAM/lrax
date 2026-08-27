b9144f37fca22f4f1bf671a727fe0a8689205e59
Evan Palmer <evanp922@gmail.com>
2026-08-27 15:46:52 -0700

    Added GRU and LSTM

--- lrax/common/networks.py
+++ lrax/common/networks.py
@@ -177,3 +177,213 @@ class TamedMLP(eqx.Module):
         A JAX array with shape `(out_size,)`.
         """
         return self.out_scale * self.mlp(x)
+
+
+class GRU(eqx.Module):
+    """A multi-layer gated-recurrent unit (GRU)."""
+
+    layers: tuple[eqx.nn.GRUCell, ...]
+    in_size: int = eqx.field(static=True)
+    hidden_size: int = eqx.field(static=True)
+    num_layers: int = eqx.field(static=True)
+
+    def __init__(
+        self,
+        in_size: int,
+        hidden_size: int,
+        num_layers: int = 1,
+        use_bias: bool = True,
+        *,
+        key: PRNGKeyArray,
+    ):
+        """Initialize the GRU.
+
+        Parameters
+        ----------
+        - `in_size`: The input size. The input to the module should be a vector of
+            shape `(seq_len, in_size)`.
+        - `hidden_size`: The size of each layer's hidden state.
+        - `num_layers`: The total number of stacked `GRUCell` units. For example:
+            - `num_layers=1` is just a single `GRUCell`
+                `[GRUCell(in_size, hidden_size)]`
+            - `num_layers=2` results in a network with layers
+                `[GRUCell(in_size, hidden_size), GRUCell(hidden_size, hidden_size)]`
+            Defaults to 1.
+        - `use_bias`: Whether to add on a bias to each cell after each update. Defaults
+            to `True`.
+        - `key`: A `jax.random.key` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+        """
+        keys = jr.split(key, num_layers)
+
+        self.in_size = in_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        init_cell = eqx.nn.GRUCell(in_size, hidden_size, use_bias, key=keys[0])
+        layers = [init_cell]
+
+        make_hidden_cell = lambda k: eqx.nn.GRUCell(
+            hidden_size, hidden_size, use_bias, key=k
+        )
+        layers.extend(make_hidden_cell(keys[i]) for i in range(1, num_layers))
+
+        self.layers = tuple(layers)
+
+    def init_state(self) -> Array:
+        """Create a new zeroed hidden state with shape `(num_layers, hidden_size)`."""
+        return jnp.zeros((len(self.layers), self.hidden_size))
+
+    def step(self, x: Array, h_x: Array) -> tuple[Array, Array]:
+        """Advance each cell by one step.
+
+        Parameters
+        ----------
+        - `x`: A JAX array of shape `(in_size,)` to provide as input to the cells.
+        - `h_x`: A JAX array of shape `(num_layers, hidden_size)` holding hidden state
+            of each layer.
+
+        Returns
+        -------
+        The top layer's hidden state and the updated network hidden state.
+        """
+        new_hidden = []
+
+        h = x
+        for i, layer in enumerate(self.layers):
+            h = layer(h, h_x[i])
+            new_hidden.append(h)
+
+        return h, jnp.stack(new_hidden)
+
+    def __call__(self, x: Array, h_0: Array) -> tuple[Array, Array]:
+        """Apply the network over an input sequence.
+
+        Parameters
+        ----------
+        - `x`: A JAX array of shape `(seq_len, in_size)` to scan the network over.
+        - `h_0`: A JAX array of shape `(num_layers, hidden_size)` holding the initial
+            hidden state of each layer.
+
+        Returns
+        -------
+        The top layer's hidden state at each element in the sequence and the final
+        hidden state of every layer.
+        """
+
+        def scan_fn(hidden: Array, x_t: Array) -> tuple[Array, Array]:
+            top, hidden = self.step(x_t, hidden)
+            return hidden, top
+
+        h_n, output = jax.lax.scan(scan_fn, h_0, x)
+        return output, h_n
+
+
+class LSTM(eqx.Module):
+    """A multi-layer long short-term memory (LSTM) network."""
+
+    layers: tuple[eqx.nn.LSTMCell, ...]
+    in_size: int = eqx.field(static=True)
+    hidden_size: int = eqx.field(static=True)
+    num_layers: int = eqx.field(static=True)
+
+    def __init__(
+        self,
+        in_size: int,
+        hidden_size: int,
+        num_layers: int = 1,
+        use_bias: bool = True,
+        *,
+        key: PRNGKeyArray,
+    ):
+        """Initialize the LSTM.
+
+        Parameters
+        ----------
+        - `in_size`: The input size. The input to the module should be a vector of
+            shape `(seq_len, in_size)`.
+        - `hidden_size`: The size of each layer's hidden and cell state.
+        - `num_layers`: The total number of stacked `LSTMCell` units. For example:
+            - `num_layers=1` is just a single `LSTMCell`
+                `[LSTMCell(in_size, hidden_size)]`
+            - `num_layers=2` results in a network with layers
+                `[LSTMCell(in_size, hidden_size), LSTMCell(hidden_size, hidden_size)]`
+            Defaults to 1.
+        - `use_bias`: Whether to add on a bias to each cell after each update. Defaults
+            to `True`.
+        - `key`: A `jax.random.key` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+        """
+        keys = jr.split(key, num_layers)
+
+        self.in_size = in_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        init_cell = eqx.nn.LSTMCell(in_size, hidden_size, use_bias, key=keys[0])
+        layers = [init_cell]
+
+        make_hidden_cell = lambda k: eqx.nn.LSTMCell(
+            hidden_size, hidden_size, use_bias, key=k
+        )
+        layers.extend(make_hidden_cell(keys[i]) for i in range(1, num_layers))
+
+        self.layers = tuple(layers)
+
+    def init_state(self) -> tuple[Array, Array]:
+        """Create a new zeroed `(hidden, cell)` state, each with shape
+        `(num_layers, hidden_size)`."""
+        zeros = jnp.zeros((len(self.layers), self.hidden_size))
+        return zeros, zeros
+
+    def step(
+        self, x: Array, h_x: tuple[Array, Array]
+    ) -> tuple[Array, tuple[Array, Array]]:
+        """Advance each cell by one step.
+
+        Parameters
+        ----------
+        - `x`: A JAX array of shape `(in_size,)` to provide as input to the cells.
+        - `h_x`: A `(hidden, cell)` 2-tuple of JAX arrays, each of shape
+            `(num_layers, hidden_size)`, holding the state of each layer.
+
+        Returns
+        -------
+        The top layer's hidden state and the updated network `(hidden, cell)` state.
+        """
+        hidden, cell = h_x
+        new_hidden, new_cell = [], []
+
+        h = x
+        for i, layer in enumerate(self.layers):
+            h, c = layer(h, (hidden[i], cell[i]))
+            new_hidden.append(h)
+            new_cell.append(c)
+
+        return h, (jnp.stack(new_hidden), jnp.stack(new_cell))
+
+    def __call__(
+        self, x: Array, h_0: tuple[Array, Array]
+    ) -> tuple[Array, tuple[Array, Array]]:
+        """Apply the network over an input sequence.
+
+        Parameters
+        ----------
+        - `x`: A JAX array of shape `(seq_len, in_size)` to scan the network over.
+        - `h_0`: A `(hidden, cell)` 2-tuple of JAX arrays, each of shape
+            `(num_layers, hidden_size)`, holding the initial state of each layer.
+
+        Returns
+        -------
+        The top layer's hidden state at each element in the sequence and the final
+        `(hidden, cell)` state of every layer.
+        """
+
+        def scan_fn(
+            hidden: tuple[Array, Array], x_t: Array
+        ) -> tuple[tuple[Array, Array], Array]:
+            top, hidden = self.step(x_t, hidden)
+            return hidden, top
+
+        h_n, output = jax.lax.scan(scan_fn, h_0, x)
+        return output, h_n
